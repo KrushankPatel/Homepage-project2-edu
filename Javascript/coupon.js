daaeee9ffd7689ba341338724d5f5863958148db
KrushankPatel <200020116052ait@gmail.com>
2023-05-04 20:13:52 +0530

    third commit

--- Javascript/coupon.js
+++ Javascript/coupon.js
@@ -1,11 +1,22 @@
 const loadCoupon = () => {
-    setTimeout(() => {
-      document.getElementById('coupon-container').style.visibility = 'visible';
-      document.getElementById('body').style.opacity = '0.7';
-    }, 2000);
-  }
+  setTimeout(() => {
+    document.getElementById('coupon-container').style.visibility = 'visible';
+    document.getElementById('body').style.opacity = '0.7';
+    document.addEventListener('click', handleOutsideClick);
+  }, 2000);
+}
 
 const closeCoupon = () =>{
-    document.getElementById('coupon-container').style.visibility = 'hidden';
-    document.getElementById('body').style.opacity = '1'
+  document.getElementById('coupon-container').style.visibility = 'hidden';
+  document.getElementById('body').style.opacity = '1';
+  document.removeEventListener('click', handleOutsideClick);
+}
+
+const handleOutsideClick = (event) => {
+  const couponContainer = document.getElementById('coupon-container');
+  const isClickedInside = couponContainer.contains(event.target);
+  
+  if (!isClickedInside) {
+    closeCoupon();
+  }
 }
